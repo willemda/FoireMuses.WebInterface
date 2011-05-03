cb38739b3b7928d735c0743a904c4a7bbaafe09c
Willem Danny <willemdanny@gmail.com>
2011-05-03 23:35:11 +0200

    First draft of the advanced search

---
+++ FoireMuses.WebInterface/Controllers/SearchController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoireMuses.Webinterface.Controllers
+{
+    public class SearchController : Controller
+    {
+        //
+        // GET: /Search/
+
+        public ActionResult Index(string type)
+        {
+            switch (type)
+            {
+                case "score":
+                    return View("SearchScore");
+                case "play":
+                    return View("SearchPlay");
+                case "music":
+                    return View("SearchMusic");
+                case "source":
+                    return View("SearchSource");
+                default:
+                    return View("SearchScore");
+            }
+        }
+
+
+
+    }
+}
